7cc8b8796408a646b445f0c0eb87624bf0f56eb0
Sokun Sovisal <sokunsovisal@gmail.com>
2018-12-19 07:34:51 +0700

    ''

--- resources/views/quotations/show.blade.php
+++ resources/views/quotations/show.blade.php
@@ -12,25 +12,24 @@
 			height: 29.7cm;
 			position: relative;
 			font-family: 'Times New Roman' !important;
-			padding: 15px 35px;
+			padding: 10px 30px;
+			font-size: 12pt;
 		}
 		.print-header{
 			padding: 22px 35px 0 38px;
-		}
-		.print-header table tr td:nth-child(2),
-		.print-header table tr td:nth-child(5){
-			border-bottom: 1px dashed #ddd;
 		}
 		.print-header .table-header{
 			padding-left: 10px;
 			margin-top: 12px;
 		}
 		.print-header table tr td{
-			padding-top: 6px;
+			padding-top: 2px;
 		}
 		.print-header h2{
 			text-align: center;
 			padding: 10px 0 5px 0;
+			text-transform: uppercase;
+			font-size: 16pt;
 		}
 		.print-header h2 strong{
 			border-bottom: 2px solid #333;
@@ -41,11 +40,13 @@
 		}
 		.print-body table.table-purpose tr td{
 			padding: 10px 0;
+			font-size: 13.5pt;
 		}
 		.print-body table.table-body tr th{
 			text-align: center;
 			padding: 7px 0;
 			border: 1px solid #000;
+			font-size: 13.5pt;
 		}
 		.print-body table.table-body tbody tr{
 			border-left: 1px solid #000;
@@ -55,10 +56,31 @@
 			border-bottom: 1px solid #000;
 			border-top: 1px solid #000;
 		}
-		.print-body table.table-body tbody tr td{
+		.print-body table.table-body tbody tr:last-child td{
 			padding: 5px 8px;
+		}
+		.print-body table.table-body tbody tr td{
+			padding: 3px 8px;
 			border-left: 1px solid #000;
 			border-right: 1px solid #000;
+		}
+		.print-body table.table-body tbody .service{
+			font-size: 12.5pt;
+			/*margin-bottom: 0px;*/
+		}
+		.print-body table.table-body tbody .service-item{
+			/*border: 1px solid red;*/
+		}
+		.print-body table.table-body tbody .service-item p{
+			/*border: 1px solid red;*/
+			margin: 5px 0 0 0;
+
+		}
+		.print-body table.table-body tbody .service-item ul{
+			/*border: 1px solid red;*/
+			margin-top: 5px;
+			margin-bottom: 5px;
+
 		}
 		.print-body table.table-body tbody tr:last-child td:last-child{
 			border: 1px solid #000;
@@ -67,12 +89,12 @@
 			text-align: center;
 		}
 		.print-body .block-signature .box{
-			height: 1.2cm;
+			height: 35px;
 		}
 		.print-footer{
 			max-width: 100%;
 			position: absolute;
-			padding: 10px 70px 22px 35px;
+			padding: 10px 65px 22px 35px;
 			bottom: 0;
 		}
 		.text-red{ color: red; }
@@ -127,29 +149,29 @@
 					
 					<table width="100%">
 						<tr>
-							<td width="10%"><strong>To</strong></td>
-							<td width="46%">{{(($quote->company->com_name=='Unkown')?'': $quote->company->com_name)}}</td>
-							<td width="8%"></td>
-							<td width="14%"><strong>Quote N&deg;</strong></td>
-							<td width="22%"><span class="text-red">{{$quote->quote_number}}</span></td>
-						</tr>
-						<tr>
-							<td width="10%"><strong>Attend</strong></td>
-							<td width="46%">{{(($quote->quote_cp_name=='Unkown')?'': $quote->quote_cp_name)}}</td>
-							<td width="8%"></td>
-							<td width="14%"><strong>Date</strong></td>
-							<td width="22%">{{$quote->quote_date}}</td>
-						</tr>
-						<tr>
-							<td width="10%"><strong>Phone</strong></td>
-							<td width="46%">{{$quote->quote_cp_phone}}</td>
+							<td width="9%"><strong>To</strong></td>
+							<td width="47%">: {{(($quote->company->com_name=='Unkown')?'': $quote->company->com_name)}}</td>
 							<td width="8%"></td>
 							<td width="14%"></td>
 							<td width="22%" class="nb"></td>
 						</tr>
 						<tr>
-							<td width="10%"><strong>E-mail</strong></td>
-							<td width="46%">{{$quote->quote_cp_email}}</td>
+							<td width="9%"><strong>Attend</strong></td>
+							<td width="47%">: {{(($quote->quote_cp_name=='Unkown')?'': $quote->quote_cp_name)}}</td>
+							<td width="8%"></td>
+							<td width="14%"><strong>Quote N&deg;</strong></td>
+							<td width="22%"><span class="text-red">: {{$quote->quote_number}}</span></td>
+						</tr>
+						<tr>
+							<td width="9%"><strong>Phone</strong></td>
+							<td width="47%">: {{$quote->quote_cp_phone}}</td>
+							<td width="8%"></td>
+							<td width="14%"><strong>Date</strong></td>
+							<td width="22%">: {{$quote->quote_date}}</td>
+						</tr>
+						<tr>
+							<td width="9%"><strong>E-mail</strong></td>
+							<td width="47%">: {{$quote->quote_cp_email}}</td>
 							<td width="8%"></td>
 							<td width="14%"></td>
 							<td width="22%" class="nb"></td>
@@ -161,7 +183,7 @@
 			<div class="print-body">
 				<table width="100%" class="table-purpose">
 					<tr>
-						<td valign="top" width="60px"><strong>Purpose: </strong></td>
+						<td valign="top" width="75px"><strong>Purpose:</strong></td>
 						<td><strong>{{$quote->quote_purpose}}</strong></td>
 					</tr>
 				</table>
@@ -169,20 +191,19 @@
 				<table width="100%" class="table-body">
 					<thead>
 						<tr>
-							<th width="5%">N&deg;</th>
-							<th width="57%">Description</th>
+							<th width="6%">N&deg;</th>
+							<th width="54%">Description</th>
 							<th width="10%">Unit</th>
-							<th width="14%">Unit Price</th>
-							<th width="14%">Ammount</th>
+							<th width="15%">Unit Price</th>
+							<th width="15%">Ammount</th>
 						</tr>
 					</thead>
 					<tbody>
 						@foreach($quotationservices as $i => $qs)
-							<tr>
+							<tr class="service-item">
 								<td align="center" valign="top">{{$i+1}}</td>
 								<td class="description">
-									<strong>{{$qs->service->s_name}}</strong>
-									<br/>
+									<strong class="service">{{$qs->service->s_name}}</strong>
 									{!! $qs->qs_description !!}
 								</td>
 								<td align="center" valign="top">{{$qs->qs_qty}}</td>
@@ -197,6 +218,7 @@
 							</tr>
 					</tbody>
 				</table>
+				<br/>
 				<div class="quote_term">{!!$quote->quote_term!!}</div>
 				<table>
 					<tr width="30%">
